80135e67e9f92fbe8f7b83e568e55850a205948f
Tsiry MANANKASINA <tsirymanankasina@gmail.com>
2024-03-11 23:36:26 +0100

    Initial commit

---
+++ src/test/java/canal/plus/subscriber/SubscriberBcApplicationTests.java
@@ -0,0 +1,13 @@
+package canal.plus.subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberBcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
