03a00097135f5a766feb0bbfbc22a4f60f721f6e
Tsiry MANANKASINA <tsirymanankasina@gmail.com>
2024-03-25 10:00:28 +0100

    move ExceptionHandler to a RestControllerAdvice

--- src/main/java/canal/plus/subscriber/controller/SubscriberController.java
+++ src/main/java/canal/plus/subscriber/controller/SubscriberController.java
@@ -8,15 +8,11 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -110,17 +106,4 @@ public class SubscriberController {
         List<Subscriber> results = subscriberRepository.findByCriteria(id, firstname, lastname, phone, mail, isActive, pageable);
         return ResponseEntity.ok(results);
     }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
-
 }
